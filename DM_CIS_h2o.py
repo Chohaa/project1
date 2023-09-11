6a97d25c8eec647fb21557cf174302c1c73160cf
hanna <89991426+Chohaa@users.noreply.github.com>
2023-09-11 13:58:36 -0400

    Update DM_CIS_h2o.py

--- DM_CIS_h2o.py
+++ DM_CIS_h2o.py
@@ -1,39 +1,69 @@
-import pyscf
-from pyscf import gto, scf, tdscf, lib, tddft
-import numpy as np
-from numpy import linalg as la
-import scipy
+import matplotlib.pyplot as plt
+from pyscf import gto, scf, mcscf, tdscf
 
-mol = gto.Mole()
-mol.atom = '''
-O          0.00000        0.00000        0.11779
-H          0.00000        0.75545       -0.47116
-H          0.00000       -0.75545       -0.47116'''
-mol.basis = '6-31g'
-mol.spin = 0
-mol.build()
+def active_space_energies(mf, norbs, nelec):
+    # Calculate natural orbitals
+    noons, natorbs = mcscf.addons.make_natural_orbitals(mf)
 
-mf = scf.RHF(mol)
-mf.verbose = 4
-mf.get_init_guess(mol, key='minao')
-mf.run()
+    energies = []  # Store the energies for different active space sizes
 
-mo = mf.mo_coeff
-mo_energy = mf.mo_energy
+    for i in range(2, norbs):
+        for j in range(2, nelec, 2):
+            ncas, nelecas = i, j
+            if ncas <= nelec <= norbs:
+                mycas = mcscf.CASCI(mf, ncas, nelecas)
+                mycas.kernel(natorbs)
 
-nocc = int(mol.nelectron/2)
-nvir = mo.shape[1]-nocc
+                energies.append(mycas.e_tot)
 
-mo_occ = mo_energy[nocc:]
-mo_vir = mo_energy[:nocc]
+    return energies
 
-print('nelec =', nocc)
-print('norb = ', len(mo))
+def main():
+    # Define the molecular geometry and build the molecule
+    mol = gto.Mole()
+    mol.atom = '''
+    O          0.00000        0.00000        0.11779
+    H          0.00000        0.75545       -0.47116
+    H          0.00000       -0.75545       -0.47116'''
+    mol.basis = '6-31g'
+    mol.spin = 0
+    mol.build()
 
-#number of electrons from density matrix 
+    # Initialize and run the RHF calculation
+    mf = scf.RHF(mol)
+    mf.verbose = 4
+    mf.get_init_guess(mol, key='minao')
+    mf.conv_tol = 1e-9
+    mf.run()
 
+    # Calculate the active space energies
+    norbs = len(mf.mo_coeff)
+    nelec = int(mol.nelectron / 2)
+    energies = active_space_energies(mf, norbs, nelec)
 
+    # Create xpoints corresponding to the active space sizes (string labels)
+    xpoints = [f"({i}, {j})" for i in range(2, norbs) for j in range(2, nelec, 2) if i <= nelec <= norbs]
 
-#average density matrix
+    # Compute TD-HF
+    n_states = 1  # Number of states to compute
+    mytd = tdscf.TDHF(mf)
+    mytd.nstates = n_states
+    mytd.kernel()
+    tdhf_energies = mytd.e * 27.2114  # Convert to eV
 
+    # Calculate the error
+    errors = [tdhf_energies - energy for energy in energies]  # Difference between TD-HF and CI energies
 
+    # Create a plot
+    plt.figure(figsize=(10, 6))
+    plt.plot(xpoints, errors, marker='o', linestyle='-')
+    plt.xlabel('Active Space Size (ncas, nelecas)')
+    plt.ylabel('Energy Error (eV)')
+    plt.title('Error in CI Energy vs. Active Space Size')
+    plt.xticks(rotation=45, ha='right')
+    plt.grid(True)
+    plt.tight_layout()
+    plt.show()
+
+if __name__ == "__main__":
+    main()
