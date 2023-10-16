e05c7ef1497b25dba183b6b925e004b3140edff1
hanna <89991426+Chohaa@users.noreply.github.com>
2023-10-16 17:07:53 -0400

    Update singly_excited_h2o_cis.py

--- singly_excited_h2o_cis.py
+++ singly_excited_h2o_cis.py
@@ -1,3 +1,11 @@
+import pyscf 
+from pyscf import gto, scf, tdscf, ao2mo
+import matplotlib.pyplot as plt
+
+import scipy
+import numpy as np
+from numpy import linalg
+
 # Define a function to calculate the density matrix
 def tda_density_matrix(td, state_id):
     cis_t1 = td.xy[state_id][0]
@@ -12,10 +20,13 @@ def tda_density_matrix(td, state_id):
     dm = np.einsum('pi,ij,qj->pq', mo, dm, mo.conj())
     return dm
 
-def get_natural_orbital_active_space(rdm, S, thresh=0.01):
+
+def get_natural_orbital_active_space(rdm, S, thresh=.01, save_lengths=False):
     Ssqrt = scipy.linalg.sqrtm((S + S.T) / 2.0)
     Sinvsqrt = scipy.linalg.inv(Ssqrt)
     print(" Number of electrons found %12.8f" % np.trace(S @ rdm))
+    #Dtot = 
+    #D_evecs = 
     Dtot = Ssqrt.T @ rdm @ Ssqrt
     D_evals, D_evecs = np.linalg.eigh((Dtot + Dtot.T) / 2.0)
     sorted_list = np.argsort(D_evals)[::-1]
@@ -23,6 +34,7 @@ def get_natural_orbital_active_space(rdm, S, thresh=0.01):
     D_evecs = D_evecs[:, sorted_list]
     act_list = []
     doc_list = []
+    vir_list = []
     for idx, n in enumerate(D_evals):
         print(" %4i = %12.8f" % (idx, n), end="")
         if n < 2.0 - thresh:
@@ -30,42 +42,49 @@ def get_natural_orbital_active_space(rdm, S, thresh=0.01):
                 act_list.append(idx)
                 print(" Active")
             else:
+                vir_list.append(idx)
                 print(" Virt")
         else:
             doc_list.append(idx)
             print(" DOcc")
-    return act_list, doc_list
+
+    print(" Number of active orbitals: ", len(act_list))
+    print(" Number of doc    orbitals: ", len(doc_list))
+    
+    if save_lengths:
+        # Save the lengths of act_list and doc_list to a text file
+        with open('list_lengths.txt', 'w') as file:
+            file.write(f"Length of act_list: {len(act_list)}")
+            file.write(f"Length of doc_list: {len(doc_list)}")
+
+    D_evecs = Sinvsqrt @ D_evecs
+    Cdoc = D_evecs[:, doc_list]
+    Cact = D_evecs[:, act_list]
+    Cvir = D_evecs[:, vir_list]
+    
+    return Cdoc, Cact, doc_list, act_list, vir_list  
+
+def semi_canonicalize(C,F):
+    e,v = np.linalg.eigh(C.conj().T @ F @ C)
+    C_bar = C @ v
+    return C_bar, e
 
 
-def active_space_energies_td(mf, norbs, nelec, n_singlets):
-    energies = []  # Store the energies for different active space sizes
-    xpoints = []  # Store labels for the x-axis
-
-    for i in range(2, norbs, 2):
-        for j in range(2, nelec, 2):
-            ncas, nelecas = i, j
-            if ncas <= norbs and nelecas <= ncas:  # Ensure valid CAS setup
-                # Check if the number of virtual orbitals is non-negative
-                nvir = norbs - nelecas
-                if nvir >= 0:
-                    mycas = mcscf.CASCI(mf, ncas, nelecas)
-                    try:
-                        mycas.kernel()
-                        energies.append(mycas.e_tot)
-                        xpoints.append(f"({ncas}, {nelecas})")
-                    except Exception as e:
-                        print(f"Error for CAS({ncas}, {nelecas}): {str(e)}")
-
-    return energies, xpoints
+def print_matrix_as_integers(mat):
+    for row in mat:
+        # Cast elements to integers, then join them as strings with proper spacing
+        row_str = " ".join(str(int(elem)) for elem in row)
+        print(row_str)
 
 
 # Create the molecule
 mol = gto.Mole()
 mol.atom = '''
-O          0.00000        0.00000        0.11779
-H          0.00000        0.75545       -0.47116
-H          0.00000       -0.75545       -0.47116'''
-mol.basis = '6-31g'
+    O   0. 0. 0.
+    H   0. 1. 0.
+    H   0. 0. 1.
+'''
+mol.unit = 'B'
 mol.basis = '6-31g'
 mol.spin = 0
 mol.build()
@@ -73,113 +92,171 @@ mol.build()
 # Perform Restricted Hartree-Fock calculation
 mf = scf.RHF(mol)
 mf.verbose = 4
-mf.get_init_guess(mol, key='minao')
-mf.kernel() 
-
-norbs = len(mf.mo_coeff)
-nelec = int(mol.nelectron)
-
-avg_rdm1 = mf.make_rdm1()
-
-# Compute singlets and triplets
-n_singlets = 1
-n_triplets = 1
-
-
-# CIS
-# compute singlets
-mytd = tdscf.TDA(mf)
-mytd.singlet = True
-mytd = mytd.run(nstates=n_singlets)
-mytd.analyze()
-for i in range(mytd.nroots):
-    avg_rdm1 += tda_density_matrix(mytd, i)
-print('TDA CIS singlet excited state total energy = ', mytd.e_tot)
-
-mfs = mytd._scf
-e_s, xpoints_singlets = active_space_energies_td(mfs,norbs, nelec, n_singlets)
-e_ciss = mytd.e_tot
-
-# Calculate the error for singlets
-errors_s = [e_ciss[0] - energy for energy in e_s]
-
-# compute triplets
-mytd = tdscf.TDA(mfs)  # Fixed variable name from mftd to mfs
-mytd.singlet = False
-mytd = mytd.run(nstates=n_triplets)
-mytd.analyze()
-for i in range(mytd.nroots):
-    avg_rdm1 += tda_density_matrix(mytd, i)
-print('TDA CIS Triplet excited state total energy = ', mytd.e_tot)
-
-# varying active space size
-mfs = mytd._scf
-e_t, xpoints_triplets = active_space_energies_td(mfs, norbs, nelec, n_triplets)
-e_cist = mytd.e_tot
-
-# Calculate the error for tripletnit_guess(mol, key='minao')
 mf.kernel()
 
-norbs = len(mf.mo_coeff)
-nelec = int(mol.nelectron / 2)
+F = mf.get_fock()
+J, K = mf.get_jk()
+#V = mf.get_veff()
+V = mf.get_veff()
 
-avg_rdm1 = mf.make_rdm1()
+S = mf.get_ovlp() # overlab matrix in AO basis
+Ssqrt = scipy.linalg.sqrtm((S + S.T) / 2.0)
+
+C = mf.mo_coeff # mo coefficient 
+P = mf.make_rdm1()
+print('rdm')
+print_matrix_as_integers(P)
+print('rdm from mo')
+print_matrix_as_integers(C @ C.T)
+
+H_core = mf.get_hcore()
+E_init = mf.e_tot
+
+Vnn = gto.mole.energy_nuc(mol)
+Vne = mol.intor_symmetric('int1e_nuc')
+T = mol.intor_symmetric('int1e_kin')
 
 # Compute singlets and triplets
 n_singlets = 1
 n_triplets = 1
 
-# CIS
+# CIS to define active space from rdm
 # compute singlets
 mytd = tdscf.TDA(mf)
 mytd.singlet = True
 mytd = mytd.run(nstates=n_singlets)
 mytd.analyze()
 for i in range(mytd.nroots):
-    avg_rdm1 += tda_density_matrix(mytd, i)
-print('TDA CIS singlet excited state total energy = ', mytd.e_tot)
+    P += tda_density_matrix(mytd, i)
+escis = mytd.e_tot
+print('escis = ', mytd.e_tot)
 
-mfs = mytd._scf
-e_s, xpoints_singlets = active_space_energies_td(mfs,norbs, nelec, n_singlets)
-e_ciss = mytd.e_tot
-
-# Calculate the error for singlets
-errors_s = [e_ciss[0] - energy for energy in e_s]
-
-# compute triplets
-mytd = tdscf.TDA(mfs)  # Fixed variable name from mftd to mfs
+# compute triplets 
+mytd = tdscf.TDA(mf)  
 mytd.singlet = False
 mytd = mytd.run(nstates=n_triplets)
 mytd.analyze()
 for i in range(mytd.nroots):
-    avg_rdm1 += tda_density_matrix(mytd, i)
-print('TDA CIS Triplet excited state total energy = ', mytd.e_tot)
+    P += tda_density_matrix(mytd, i)
+etcis = mytd.e_tot
+print('etcis = ', mytd.e_tot)
 
-# varying active space size
-mfs = mytd._scf
-e_t, xpoints_triplets = active_space_energies_td(mfs, norbs, nelec, n_triplets)
-e_cist = mytd.e_tot
+# New density matrix
+P = P / (n_singlets + n_triplets + 1)
+Cdoc, Cact, doc_list, act_list, vir_list = get_natural_orbital_active_space(P, S, thresh=0.01, save_lengths=True)
 
-# Calculate the error for triplets
-errors_t = [e_cist - energy for energy in e_t]
+Cenv = C[:, doc_list]
+Cact = C[:, act_list]
+Cvir = C[:, vir_list]
 
-# new density matrix
-avg_rdm1 = avg_rdm1 / (n_singlets + n_triplets + 1)
-# Compute natural orbital
-Cdoc, Cact = get_natural_orbital_active_space(avg_rdm1, mf.get_ovlp(), thresh=0.00275)
+nact = Cact.shape[1]
+nenv = Cenv.shape[1]
+nvir = Cvir.shape[1]
 
-print('TDA CIS singlet excited state total energy = ', e_ciss)
-print('TDA CIS Triplet excited state total energy = ', e_cist)
+print('Number of Active AO: ', nact)
+print('Number of Environment MOs: ', nenv)
+print('Number of Virtual MOs: ', nvir)
 
+D_C = 2 * Cvir @ Cvir.conj().T
+D_B = 2 * Cenv @ Cenv.conj().T
+D_A = 2 * Cact @ Cact.conj().T
+
+#values for subspace, new mf
+Jenv, Kenv = mf.get_jk(dm = D_B)
+Jact, Kact = mf.get_jk(dm = D_A)
+
+Vact = Jact - 0.5 * Kact
+Venv = Jenv - 0.5 * Kenv
+
+P_B = S @ D_B @ S + S @ D_C @ S
+mu = 10^6
+
+emb = (J - 0.5 * K) - Venv + (mu * P_B)
+H_core += emb
+
+'''
+#DFT
+Venv = mf.get_veff(dm=D_B)
+Vact = mf.get_veff(dm=D_A)
+V = mf.get_veff()
+'''
+mol = gto.Mole()
+mol.atom = '''
+    O   0. 0. 0.
+    H   0. 1. 0.
+    H   0. 0. 1.
+'''
+mol.unit = 'B'
+mol.basis = '6-31g'
+mol.spin = 0
+emb_mf = scf.RHF(mol)
+
+mol.nelectron = int(np.trace(S@P))
+emb_mf.verbose = 4
+emb_mf.get_hcore =  lambda *args: H_core
+emb_mf.max_cycle = 200
+emb_mf.kernel(dm0=D_A)
+
+S_emb = emb_mf.get_ovlp(mol)
+F_emb = emb_mf.get_fock()
+C_emb = emb_mf.mo_coeff
+P_emb = emb_mf.make_rdm1
+J_emb, K_emb = emb_mf.get_jk()
+V_emb = J_emb - 0.5 *K_emb
+
+
+E_env = np.einsum('ij,ji->', D_B, H_core) + 0.5 * np.einsum('ij,ji->', D_B, Venv)
+E_act = np.einsum('ij,ji->', D_A, H_core) + 0.5 * np.einsum('ij,ji->', D_A, Vact)
+E_cross = 0.5 * np.einsum('ij,ji->', D_B, Vact) + 0.5 * np.einsum('ij,ji->', D_A, Venv)
+
+E_emb = np.einsum('ij,ji->', P_emb, mf.get_hcore) + 0.5 * np.einsum('ij,ji->', P_emb, V_emb)
+correction = np.einsum('ij,ji->', V_emb, (P_emb-D_A))
+post = mu * np.einsum('ij,ji->', self.P_emb, P_B )
+
+embed_SCF = E_emb + E_env + E_cross + correction + emb_mf.Vnn + post
+
+print('sanitycheck', embed_SCF - E_init)
+
+# CIS
+# compute singlets
+mf_eff = tdscf.TDA(emb_mf)
+mf_eff.singlet = True
+mf_eff = mf_eff.run(n_states =1)
+mf_eff.analyze()
+print('e_ciss = ', mf_eff.e_tot)
+e_ciss = mf_eff.e_tot
+errors_s = escis - e_ciss
+# varying active space size singlet
+
+
+# compute triplets 
+mf_eff = tdscf.TDA(emb_mf)  
+mf_eff.singlet = False
+mf_eff = mf_eff.run(n_states = 1)
+mf_eff.analyze()
+e_cist = mf_eff.e_tot
+errors_t = etcis - e_cist
+print('e_cist = ', mf_eff.e_tot)
+
+# varying active space size 
+
+
+
+print('number of orbitals, electrons =', mol.nao, mol.nelectron)
+print('TDA CIS singlet excited state total energy in active space = ', e_ciss)
+print('TDA CIS triplet excited state total energy in active space= ', e_cist)
+'''
 # Create a plot
 plt.figure(figsize=(10, 6))
 
 # Plot the errors for singlets if there are singlet states
 if errors_s:
+    xpoints_singlets = 1
     plt.plot(xpoints_singlets, errors_s, marker='o', linestyle='-', label='Singlets')
 
 # Plot the errors for triplets if there are triplet states
 if errors_t:
+    xpoints_triplets = 1
     plt.plot(xpoints_triplets, errors_t, marker='o', linestyle='-', label='Triplets')
 
 plt.xlabel('Active Space Size (ncas, nelecas)')
@@ -189,34 +266,6 @@ plt.xticks(rotation=45, ha='right')
 plt.grid(True)
 plt.legend()
 plt.tight_layout()
+plt.show()
 plt.savefig('ttc_cis_activespace.png')
-
-errors_t = [e_cist - energy for energy in e_t]
-
-# new density matrix
-avg_rdm1 = avg_rdm1 / (n_singlets + n_triplets + 1)
-# Compute natural orbital
-Cdoc, Cact = get_natural_orbital_active_space(avg_rdm1, mf.get_ovlp(), thresh=0.00275)
-
-print('TDA CIS singlet excited state total energy = ', e_ciss)
-print('TDA CIS Triplet excited state total energy = ', e_cist)
-
-# Create a plot
-plt.figure(figsize=(10, 6))
-
-# Plot the errors for singlets if there are singlet states
-if errors_s:
-    plt.plot(xpoints_singlets, errors_s, marker='o', linestyle='-', label='Singlets')
-
-# Plot the errors for triplets if there are triplet states
-if errors_t:
-    plt.plot(xpoints_triplets, errors_t, marker='o', linestyle='-', label='Triplets')
-
-plt.xlabel('Active Space Size (ncas, nelecas)')
-plt.ylabel('Energy difference; CIS - active space (eV)')
-plt.title('CI Energy vs. Active Space Size')
-plt.xticks(rotation=45, ha='right')
-plt.grid(True)
-plt.legend()
-plt.tight_layout()
-plt.savefig('ttc_cis_activespace.png')
+'''
