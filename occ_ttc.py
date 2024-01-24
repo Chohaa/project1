f8ab3d684ef299912cf7728c9a147b5fc99affb6
hanna <89991426+Chohaa@users.noreply.github.com>
2024-01-24 13:39:49 -0500

    Update occ_ttc.py

--- occ_ttc.py
+++ occ_ttc.py
@@ -15,16 +15,36 @@ def print_matrix_as_integers(mat):
 # Create the molecule
 mol = gto.Mole()
 mol.atom = '''
-C         -2.30986        0.50909        0.01592
-C         -0.98261        0.43259       -0.05975
-C         -0.26676       -0.68753        0.57690
-C          1.06323       -0.78274        0.51273
-H         -2.84237        1.33139       -0.45081
-H         -2.89518       -0.26464        0.20336
-H         -0.43345        1.20285       -0.59462
-H         -0.82592       -1.45252        1.10939
-H          1.65570       -0.03776       -0.01031
-H          1.57073       -1.61569        0.98847
+H       -3.4261000000     -2.2404000000      5.4884000000
+H       -5.6274000000     -1.0770000000      5.2147000000
+C       -3.6535000000     -1.7327000000      4.5516000000
+H       -1.7671000000     -2.2370000000      3.6639000000
+C       -4.9073000000     -1.0688000000      4.3947000000
+H       -6.1631000000      0.0964000000      3.1014000000
+C       -2.7258000000     -1.7321000000      3.5406000000
+H       -0.3003000000      1.0832000000     -5.2357000000
+C       -5.2098000000     -0.4190000000      3.2249000000
+C       -2.9961000000     -1.0636000000      2.3073000000
+H       -1.1030000000     -1.5329000000      1.3977000000
+H       -0.4270000000     -0.8029000000     -0.8566000000
+H        0.2361000000     -0.0979000000     -3.1273000000
+C       -1.0193000000      1.0730000000     -4.4150000000
+H       -2.4988000000      2.2519000000     -5.5034000000
+C       -4.2740000000     -0.3924000000      2.1445000000
+H       -5.5015000000      0.7944000000      0.8310000000
+C       -2.0613000000     -1.0272000000      1.2718000000
+C       -1.3820000000     -0.2895000000     -0.9772000000
+C       -0.7171000000      0.4180000000     -3.2476000000
+C       -2.2720000000      1.7395000000     -4.5690000000
+H       -4.1576000000      2.2412000000     -3.6787000000
+C       -4.5463000000      0.2817000000      0.9534000000
+C       -2.3243000000     -0.3402000000      0.0704000000
+C       -1.6528000000      0.3874000000     -2.1670000000
+C       -3.1998000000      1.7341000000     -3.5584000000
+C       -3.6044000000      0.3309000000     -0.0943000000
+C       -2.9302000000      1.0591000000     -2.3292000000
+C       -3.8665000000      1.0187000000     -1.2955000000
+H       -4.8243000000      1.5256000000     -1.4217000000
 '''
 mol.basis = 'sto-3g'
 mol.spin = 0
@@ -48,16 +68,18 @@ C = mf.mo_coeff
 Cocc = C[:,mo_occ>0]
 D = mf.make_rdm1()
 
-molden.from_mo(mol, 'BD_mo.molden', C)
+molden.from_mo(mol, 'BD_mo.molden',C)
 
 num_orbitals = len(C)
 
 X = scipy.linalg.sqrtm(S)
 Xinv = np.linalg.inv(X)
 
+
 #orthogonalization; Canonical basis diagonal F
 C = X @ C
 F = C.conj().T @ F @ C
+
 
 print(" Number of electrons found %12.8f" %np.trace(S @ D))
 print('Number of Orbitals: ', num_orbitals )
@@ -105,7 +127,7 @@ active_sizes = []
 e000 = []
 e111 = []
 
-for i in range(lumo_index+1,num_orbitals):
+for i in range(lumo_index,num_orbitals+1):
 
     active_sizes.append(i)
 
@@ -115,44 +137,31 @@ for i in range(lumo_index+1,num_orbitals):
     act_array = np.array(act_list)
 
     Cact = C[:, act_list]
-    print('shape Cact', Cact.shape)
     act_occ = mo_occ[act_list]
     act_occ = np.array(act_occ)
-
-    print('Cact,act_occ')
-    print_matrix_as_integers(Cact)
-    print('act_occ',act_occ, act_occ.shape)
 
     Cvir = C[:, vir_list]
     vir_occ = mo_occ[vir_list]
     vir_occ = np.array(vir_occ)
-    print('Cvir,vir_occ')
-    print_matrix_as_integers(Cvir)
-    print(vir_occ, vir_occ.shape)
 
     nact = len(act_list)
     nvir = len(vir_list)
 
     D_A = np.dot(Cact*act_occ, Cact.conj().T)
-    print('DA')
-    print_matrix_as_integers(D_A)
-    #D_B = 2.0 * Cenv @ Cenv.conj().T
     D_C = np.dot(Cvir*vir_occ, Cvir.conj().T)
-    print_matrix_as_integers(D_C)
     D_tot = D_A +D_C
-    print_matrix_as_integers(D-D_tot)
-
 
     #projector
-    P_B = S @ (D_C) @ S
-    mu = 1.0e8
+    P_B = S @ Xinv @ (D_C) @ Xinv @ S
+    mu = 1.0e6
 
-    Venv = mf.get_veff(dm = D_C)
-    Vact = mf.get_veff(dm = D_A) 
+    Vsys = mf.get_veff(dm=D_tot)
+    Vact = mf.get_veff(dm=D_A) 
+    Venv = mf.get_veff(dm=D_C)
+
     #new fock ao
     Vemb = Vsys - Vact + (mu * P_B)
     verror = Vsys - Vact
-    #print('verror',verror)
 
 
     n_act = 2*round(0.5 * np.trace(D_A))
@@ -171,7 +180,6 @@ for i in range(lumo_index+1,num_orbitals):
     emb_mf.kernel(dm0=D_A)
 
     eerror = emb_mf.e_tot - mf.e_tot
-    print('eerror', eerror)
 
     #molden.from_mo(mol, 'BDocc__cact.molden', )  
 
@@ -232,7 +240,7 @@ plt.grid(True)
 plt.legend()
 plt.tight_layout()
 
-file_name = "BD_allocc.png"
+file_name = "ttc_allocc.png"
 plt.savefig(file_name)
 
 plt.show()
