8bbe3bde852fddfa2703d5f06ea6ac4e39a79d98
hanna <89991426+Chohaa@users.noreply.github.com>
2024-01-24 07:37:30 -0500

    Update occ_ttc.py

--- occ_ttc.py
+++ occ_ttc.py
@@ -1,76 +1,68 @@
 import pyscf 
-from pyscf import gto, scf, tdscf, ao2mo
+from pyscf import gto, scf, tdscf
 from pyscf.tools import molden
 import matplotlib.pyplot as plt
 
 import numpy as np
+import scipy
 
 def print_matrix_as_integers(mat):
     for row in mat:
         row_str = " ".join(str(int(elem)) for elem in row)
         print(row_str)
 
+
 # Create the molecule
 mol = gto.Mole()
 mol.atom = '''
-H       -3.4261000000     -2.2404000000      5.4884000000
-H       -5.6274000000     -1.0770000000      5.2147000000
-C       -3.6535000000     -1.7327000000      4.5516000000
-H       -1.7671000000     -2.2370000000      3.6639000000
-C       -4.9073000000     -1.0688000000      4.3947000000
-H       -6.1631000000      0.0964000000      3.1014000000
-C       -2.7258000000     -1.7321000000      3.5406000000
-H       -0.3003000000      1.0832000000     -5.2357000000
-C       -5.2098000000     -0.4190000000      3.2249000000
-C       -2.9961000000     -1.0636000000      2.3073000000
-H       -1.1030000000     -1.5329000000      1.3977000000
-H       -0.4270000000     -0.8029000000     -0.8566000000
-H        0.2361000000     -0.0979000000     -3.1273000000
-C       -1.0193000000      1.0730000000     -4.4150000000
-H       -2.4988000000      2.2519000000     -5.5034000000
-C       -4.2740000000     -0.3924000000      2.1445000000
-H       -5.5015000000      0.7944000000      0.8310000000
-C       -2.0613000000     -1.0272000000      1.2718000000
-C       -1.3820000000     -0.2895000000     -0.9772000000
-C       -0.7171000000      0.4180000000     -3.2476000000
-C       -2.2720000000      1.7395000000     -4.5690000000
-H       -4.1576000000      2.2412000000     -3.6787000000
-C       -4.5463000000      0.2817000000      0.9534000000
-C       -2.3243000000     -0.3402000000      0.0704000000
-C       -1.6528000000      0.3874000000     -2.1670000000
-C       -3.1998000000      1.7341000000     -3.5584000000
-C       -3.6044000000      0.3309000000     -0.0943000000
-C       -2.9302000000      1.0591000000     -2.3292000000
-C       -3.8665000000      1.0187000000     -1.2955000000
-H       -4.8243000000      1.5256000000     -1.4217000000
+C         -2.30986        0.50909        0.01592
+C         -0.98261        0.43259       -0.05975
+C         -0.26676       -0.68753        0.57690
+C          1.06323       -0.78274        0.51273
+H         -2.84237        1.33139       -0.45081
+H         -2.89518       -0.26464        0.20336
+H         -0.43345        1.20285       -0.59462
+H         -0.82592       -1.45252        1.10939
+H          1.65570       -0.03776       -0.01031
+H          1.57073       -1.61569        0.98847
 '''
-mol.basis = '6-31g'
+mol.basis = 'sto-3g'
 mol.spin = 0
 mol.build()
 
-Vnn = gto.mole.energy_nuc(mol)
-Vne = mol.intor_symmetric('int1e_nuc')
-T = mol.intor_symmetric('int1e_kin')
-
 # Perform Restricted Hartree-Fock calculation
-mf = scf.RHF(mol)
-mf.verbose = 4
-mf.kernel()
+mf = scf.RHF(mol).run()
 
 H_core = mf.get_hcore()
 E_init = mf.e_tot
+print(" Hartree-Fock Energy: %12.8f" % mf.e_tot)
 
 F = mf.get_fock()
 J, K = mf.get_jk()
 Vsys = mf.get_veff()
-
 mo_occ = mf.get_occ()
+mo_energy = mf.mo_energy
+
 S = mf.get_ovlp()
 C = mf.mo_coeff
-P = mf.make_rdm1()
+Cocc = C[:,mo_occ>0]
+D = mf.make_rdm1()
 
-print(" Number of electrons found %12.8f" %np.trace(S@P))
+molden.from_mo(mol, 'BD_mo.molden', C)
 
+num_orbitals = len(C)
+
+X = scipy.linalg.sqrtm(S)
+Xinv = np.linalg.inv(X)
+
+#orthogonalization; Canonical basis diagonal F
+C = X @ C
+F = C.conj().T @ F @ C
+
+print(" Number of electrons found %12.8f" %np.trace(S @ D))
+print('Number of Orbitals: ', num_orbitals )
+
+#CIS
 mftda = tdscf.TDA(mf)
 mftda.singlet = True
 mftda.run(nstates=1)
@@ -89,114 +81,124 @@ e1 = mftda.e
 print('cis excitation energy:', e1)
 print('cis triplet total energy:', ecist)
 
-molden.from_mo(mol, 'ttc.molden', C)
 
-orbital_energies = mf.mo_energy
-nelec = mol.nelectron
-num_orbitals = len(orbital_energies)
+act_list = []
+doc_list = []
 
-# Calculate HOMO and LUMO indices from mo_occf
+# Calculate HOMO and LUMO indices from mo_occ
 homo_index = np.where(mo_occ == 2)[0][-1]
 lumo_index = homo_index + 1
+
+print('homoidx',homo_index)
+print('lumoidx',lumo_index)
 
 #active_sizes = len(lumo_index,num_orbitals)
 #active_sizes = list(range(0,active_sizes))
 
-e_ciss_list = []
-e_cist_list = []
+estot_list = []
+ettot_list = []
 
-#error_s = []  
-#error_t = []  
+toterror_s = []  
+toterror_t = []  
 
+active_sizes = []
 e000 = []
 e111 = []
 
-active_sizes = []
+for i in range(lumo_index+1,num_orbitals):
 
-for i in range(lumo_index,num_orbitals):
+    active_sizes.append(i)
 
-    active_sizes.append(i+1)
-
-    act_list = list(range(0, i+1))
-    vir_list = list(range(i+1, num_orbitals))
+    act_list = list(range(0, i))
+    vir_list = list(range(i, num_orbitals))
 
     act_array = np.array(act_list)
 
     Cact = C[:, act_list]
+    print('shape Cact', Cact.shape)
+    act_occ = mo_occ[act_list]
+    act_occ = np.array(act_occ)
+
+    print('Cact,act_occ')
+    print_matrix_as_integers(Cact)
+    print('act_occ',act_occ, act_occ.shape)
+
     Cvir = C[:, vir_list]
+    vir_occ = mo_occ[vir_list]
+    vir_occ = np.array(vir_occ)
+    print('Cvir,vir_occ')
+    print_matrix_as_integers(Cvir)
+    print(vir_occ, vir_occ.shape)
 
-    pyscf.tools.molden.from_mo(mol, "Cact.molden", Cact)
+    nact = len(act_list)
+    nvir = len(vir_list)
 
-    nact = Cact.shape[1]
-    nvir = Cvir.shape[1]
-
-    print('Number of Active AO: ', nact)
-    print('Number of Virtual MOs: ', nvir)
-
-    D_A = 2.0 * Cact @ Cact.conj().T
+    D_A = np.dot(Cact*act_occ, Cact.conj().T)
+    print('DA')
+    print_matrix_as_integers(D_A)
     #D_B = 2.0 * Cenv @ Cenv.conj().T
-    D_C = 2.0 * Cvir @ Cvir.conj().T
+    D_C = np.dot(Cvir*vir_occ, Cvir.conj().T)
+    print_matrix_as_integers(D_C)
+    D_tot = D_A +D_C
+    print_matrix_as_integers(D-D_tot)
 
-    #values for subspace, new mf
-    h0 = gto.mole.energy_nuc(mol)
-    Jenv, Kenv = mf.get_jk(dm = D_C)
 
-    #integrals h0 (E_0)
-    h0 += np.trace(D_C @ (H_core + 0.5 * Jenv - 0.25 * Kenv ))
+    #projector
+    P_B = S @ (D_C) @ S
+    mu = 1.0e8
 
-    P_B = S @ (D_C)@ S
-    mu = 1.0e6
-
-    Venv = mf.get_veff(D_C)
-    Vact = mf.get_veff(D_A)
-
+    Venv = mf.get_veff(dm = D_C)
+    Vact = mf.get_veff(dm = D_A) 
     #new fock ao
     Vemb = Vsys - Vact + (mu * P_B)
+    verror = Vsys - Vact
+    #print('verror',verror)
 
-    na_act = 0.5*np.trace(Cact.conj().T @ S @ P @ S @ Cact)
-    nb_act = 0.5*np.trace(Cact.conj().T @ S @ P @ S @ Cact)
 
-    na_act = round(na_act)
-    nb_act = round(nb_act)
-    n_elec = na_act + nb_act
+    n_act = 2*round(0.5 * np.trace(D_A))
+
+    print('Number of Active orbitals: ', nact)
+    print('Number of Virtual orbitals: ', nvir)
+    print('Number of electrons in active space',n_act)
 
     emb_mf = scf.RHF(mol)
-    mol.nelectron = n_elec
-
+    mol.nelectron = n_act
     mol.build()
 
-    emb_mf.verbose = 5
+    emb_mf.verbose = 4
     emb_mf.get_hcore = lambda *args: H_core + Vemb
     emb_mf.max_cycle = 200
-    emb_mf.kernel(D_A)
+    emb_mf.kernel(dm0=D_A)
+
+    eerror = emb_mf.e_tot - mf.e_tot
+    print('eerror', eerror)
+
+    #molden.from_mo(mol, 'BDocc__cact.molden', )  
 
     # CIS calculations for singlets and triplets
     es_eff = tdscf.TDA(emb_mf)
     es_eff.singlet = True
     es_eff.run(nstates=3)
     es_eff.analyze()
-    e_ciss = min(es_eff.e_tot)
+    estot = min(es_eff.e_tot)
     e00 = min(es_eff.e)
-    print('cis excitation energy:', e00)
-    print('cis total energy:', e_ciss)
+
 
     et_eff = tdscf.TDA(emb_mf)
     et_eff.singlet = False
     et_eff.run(nstates=3)
     et_eff.analyze()
-    e_cist = min(et_eff.e_tot)
+    ettot = min(et_eff.e_tot)
     e11 = min(et_eff.e)
-    print('cis excitation energy:', e11)
-    print('cis total energy:', e_cist)
-
-    e_ciss_list.append(e_ciss)
-    e_cist_list.append(e_cist)
 
     e000.append(e00)
     e111.append(e11)
 
-    #error_s.append(e_ciss - eciss)
-    #error_t.append(e_cist - ecist)
+    estot_list.append(estot)
+    ettot_list.append(ettot)
+
+    toterror_s.append(estot - eciss)
+    toterror_t.append(ettot - ecist)
 
 
 plt.figure(figsize=(10, 6))
@@ -205,31 +207,32 @@ plt.figure(figsize=(10, 6))
 plt.subplot(1, 2, 1)
 plt.plot(active_sizes, e000, marker='o', linestyle='-', label='Singlet')
 plt.plot(active_sizes, e111, marker='o', linestyle='-', label='Triplet')
-plt.axhline(y=e0, color='red', linestyle='--', label='CIS Singlet')
-plt.axhline(y=e1, color='blue', linestyle='--', label='CIS Triplet')
+plt.axhline(y=e0, color='blue', linestyle='--', label='CIS Singlet')
+plt.axhline(y=e1, color='red', linestyle='--', label='CIS Triplet')
 
-plt.xlabel('Active Space Size')
+
+plt.xlabel('# orbitals in active space')
 plt.ylabel('Excitation energy (in eV)')
-plt.title('Excitation energy of active spaces')
+plt.title('Excitation energy of active space')
 plt.grid(True)
 plt.legend()
 plt.tight_layout()
 
 # Create the second subplot
 plt.subplot(1, 2, 2)
-plt.plot(active_sizes, e_ciss_list, marker='o', linestyle='-', label='Singlet')
-plt.plot(active_sizes, e_cist_list, marker='o', linestyle='-', label='Triplet')
+plt.plot(active_sizes, estot_list, marker='o', linestyle='-', label='Singlet')
+plt.plot(active_sizes, ettot_list, marker='o', linestyle='-', label='Triplet')
 plt.axhline(y=eciss, color='red', linestyle='--', label='CIS Singlet')
 plt.axhline(y=ecist, color='blue', linestyle='--', label='CIS Triplet')
 
-plt.xlabel('Active Space Size')
+plt.xlabel('# orbitals in active space')
 plt.ylabel('Total energy(hartree)')
-plt.title('Total energy of active spaces ')
+plt.title('Total energy of active space_OCC')
 plt.grid(True)
 plt.legend()
 plt.tight_layout()
 
-file_name = "ttc_allocc.png"
+file_name = "BD_allocc.png"
 plt.savefig(file_name)
 
 plt.show()
